2f6ac6bd9c15532f48fdb01581ac0441007cd3b1
Wang Zihao <e0424629@u.nus.edu>
2023-12-07 13:16:13 +0800

    Update mpc.py

--- mpc.py
+++ mpc.py
@@ -183,7 +183,7 @@ def eval_f(x):
     for j in range (int(NUM_MPC_INPUTS/2)):
         est_outlet_product = df_ensemble_output[j, :, 0:2]
         for i in range (int(NUM_IN_SEQUENCE/short_factor)):  #NUM_IN_SEQUENCE/2
-             offset = offset + (setpoint[0] - (est_outlet_product[i, 0]))  + (setpoint[1] - (est_outlet_product[i, 1])) * 1000
+             offset = offset + (setpoint[0] - (est_outlet_product[i, 0])) ** 2  + (setpoint[1] - (est_outlet_product[i, 1])) ** 2 * 1000
         offset=offset+x[2*j] ** 2 *3e-10 + 1* x[2*j+1] ** 2
 
     return offset/100
